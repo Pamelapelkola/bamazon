4daa120b5214573214e7f82c656ed5fb1706a0b7
Pam Pelkola <pelkola.pam@gmail.com>
2018-05-07 12:38:34 -0500

    finalized shopping cart total

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,14 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-
+const keys = require("./keys");
+const pw = keys.password.pw;
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-
     user: "root",
-
-    password: "Tyler123!",
+    password: pw,
+    
     database: "bamazon_db"
 });
 connection.connect(function (err) {
@@ -19,17 +19,18 @@ connection.connect(function (err) {
 function shop() {
     connection.query("SELECT * FROM products", function (err, results) {
             if (err) throw err;
-    
+            var selectedqty;
             inquirer
                 .prompt([
                     {
                     name: 'item',
-                    type: 'list',
+                    type: 'rawlist',
                     message: 'Which item would you like to purchase?',
                     choices: function () {
                         var itemsArray = [];
                         for (var i = 0; i < results.length; i++) {
-                            itemsArray.push(results[i].product_name);
+                           
+                            itemsArray.push(`${results[i].item_id} || ${results[i].product_name}  || ${results[i].product_name}`);
                         }
                         return itemsArray;
                     },
@@ -43,60 +44,68 @@ function shop() {
                               return true;
                             }
                             return false;
-                        }
                     }
+                }
                  ]) 
                     .then(function (answer) {
-                    var chosenProduct;
-                    for (var i = 0; i < results.length; i++) {
-                        if (results[i].stock_qty === answer.qty) {
-                            chosenProduct = results[i];
-                        }
+                    var chosenProduct = '';
+                    for(var i = 0; i < 4; i++){
+                        chosenProduct += answer.item[i]
+                        
                     }
+                   
+                    var set = {item_id:chosenProduct}
+                    var sql = "SELECT * FROM products WHERE ?";
+                    sql = mysql.format(sql, set);
+                    var resobject = {}
+
+                    connection.query({sql}, function (err, newresults) {
+                    resobject.item_id = newresults[0].item_id
+                    resobject.product_name = newresults[0].product_name
+                    resobject.department_name = newresults[0].department_name
+                    resobject.stock_qty = newresults[0].stock_qty
+                    resobject.price = newresults[0].price
+                    console.log(resobject.product_name);
+                    var newqty = resobject.stock_qty-answer.qty 
+                    var newset = {stock_qty:newqty}
+                    var where = {item_id:resobject.item_id}
+                    var inserts = [newset, where]
+                    var sql = "UPDATE products SET ? WHERE ?";
+                    sql = mysql.format(sql, inserts);
+
+                    // connection.query({sql}, function (err, updateresults) {
+                      
+                    // })
+                    
+                    var priceqty = resobject.price*answer.qty
+                    var priceset = {price:priceqty}
+                    var pricewhere = {item_id:resobject.item_id}
+                    
+                    resobject.price = priceqty
+
+                    if (answer.qty > resobject.stock_qty){
+                        console.log('Sorry insufficient quantity');
+                        shop();
+                    }
+                    
+                    
+                    else{
+                    console.log("Your cart total is $" + priceqty);
+                        
+                    }
+                    }) 
+                  
                 })
-            }
-            // howManyAvail();
-        )}
+            })
+        }
+        
+        
+          
+  
 
-    // function howManyAvail(){
-    //     connection.query("SELECT * FROM products WHERE stock_qty = ?", function (err, results){
-    //         if (err) throw err;
-    //     })
-    //     for (var i = 0; i < results.length; i++){
-    //         var qtyArray = [];
-    //         if(results[i].stock_qty <= 0)
-    //         return "This item is out of stock"
-
-    //     }else{
-    //         connection.query(
-    //             "UPDATE stock_qty SET ? WHERE ?",
-    //             [
-    //                 {
-
-    //                 },
-    //                 {
-
-    //                 },
-    //             ]
-    //         function()
-    //     }
    
-    //                 var qtyArray = [];
-    //                 for (var i = 0; i < results.length; i++) {
-    //                     qtyArray.push(results[i].);
-    //                 }
-    //                 return itemsArray;
-    //             },
-    //             message: "How many would you like?",
-    //             validate: function(value) {
-    //               if (isNaN(value) === false) {
-    //                 return true;
-    //               }
-    //               return false;
-    //             }
-    //         )]
-         
+ 
+    
         
 
     
-  
